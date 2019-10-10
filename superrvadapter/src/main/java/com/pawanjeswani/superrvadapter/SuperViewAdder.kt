e02359d9af11332cfc284ff6001e8d2185818125
yewintnaing <yewintnaing44@gmail.com>
2019-10-10 13:44:20 +0630

    add documentation in super view adder

--- superrvadapter/src/main/java/com/pawanjeswani/superrvadapter/SuperViewAdder.kt
+++ superrvadapter/src/main/java/com/pawanjeswani/superrvadapter/SuperViewAdder.kt
@@ -14,9 +14,20 @@ class SuperViewAdder<B> {
     private var otherViewPositions: MutableList<DummyObject<B>> = ArrayList()
 
 
+    /**
+     * To add data object to specific position for displaying in view
+     *
+     * @param position This is location(Int start from 0) where you want to add
+     * @param data data object for displaying in view at the specified position
+     */
     fun addDataToPosition(position: Int, data: B) {
         otherViewPositions.add(DummyObject(position, data = data))
     }
 
+    /**
+     * get  view positions  that is added from  addDataToPosition(pos,data)
+     *
+     * @return view positions with data object
+     */
     fun getOtherViewPositions(): MutableList<DummyObject<B>> = otherViewPositions
 }
